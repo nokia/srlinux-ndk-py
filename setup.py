164c474f7e6088b20da36a31628959147ea3af13
Roman Dodin <dodin.roman@gmail.com>
2023-03-06 22:05:16 +0100

    release v0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ __author__ = "Roman Dodin <dodin.roman@gmail.com>"
 
 setup(
     name="srlinux-ndk",
-    version="0.1.1-rc1",
+    version="0.1.1",
     packages=["ndk"],
     author="Nokia",
     author_email="roman.dodin@nokia.com",
